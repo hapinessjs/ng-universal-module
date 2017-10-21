d9240a3bacd3d9e36518d8921443cbbea11a8611
njl07 <akanass@icloud.com>
2017-10-21 19:04:20 +0200

    feat(test): NgUniversalModule unit tests

---
+++ test/unit/ng-universal.module.test.ts
@@ -0,0 +1,67 @@
+import { test, suite } from 'mocha-typescript';
+import { NgUniversalModule } from '../../src';
+
+import * as unit from 'unit.js';
+
+@suite('- Unit NgUniversalModuleTest file')
+export class NgUniversalModuleTest {
+    /**
+     * Function executed before the suite
+     */
+    static before() {
+    }
+
+    /**
+     * Function executed after the suite
+     */
+    static after() {
+    }
+
+    /**
+     * Class constructor
+     * New lifecycle
+     */
+    constructor() {
+    }
+
+    /**
+     * Function executed before each test
+     */
+    before() {
+    }
+
+    /**
+     * Function executed after each test
+     */
+    after() {
+    }
+
+    /**
+     * Test if `NgUniversalModule` as a `setConfig` static function
+     */
+    @test('- `NgUniversalModule` must have `setConfig` static function')
+    testNgUniversalModuleSetConfig() {
+        unit.function(NgUniversalModule.setConfig);
+    }
+
+    /**
+     * Test if `NgUniversalModule.universal()` static function returns CoreModuleWithProviders
+     */
+    @test('- `NgUniversalModule.setConfig()` static function must return CoreModuleWithProviders')
+    testNgUniversalModuleSetConfigReturnsCoreModuleWithProviders(done) {
+        const cwp = NgUniversalModule.setConfig({bootstrap: <any> {}});
+        unit.object(cwp)
+            .hasProperty('module')
+            .hasProperty('providers')
+            .when(_ => {
+                unit.array(cwp.providers)
+                    .hasLength(1)
+                    .when(__ => {
+                        unit.object(cwp.providers.pop())
+                            .hasProperty('provide')
+                            .hasProperty('useValue');
+                        done();
+                    });
+            });
+    }
+}
