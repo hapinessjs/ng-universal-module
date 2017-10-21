345053771591e1eeb5300521c3ecadb3fad87b72
njl07 <akanass@icloud.com>
2017-10-21 21:41:39 +0200

    feat(test): NgUniversalModule integration tests

---
+++ test/integration/ng-universal.module.test.ts
@@ -0,0 +1,81 @@
+import { test, suite } from 'mocha-typescript';
+import { Hapiness, HapinessModule, HttpServerExt } from '@hapiness/core';
+import { NgUniversalModule } from '../../src';
+
+import * as unit from 'unit.js';
+
+@suite('- Integration NgUniversalModuleTest file')
+export class NgUniversalModuleTest {
+    /**
+     * Function executed before the suite
+     */
+    static before() {
+    }
+
+    /**
+     * Function executed after the suite
+     */
+    static after() {
+    }
+
+    /**
+     * Class constructor
+     * New lifecycle
+     */
+    constructor() {
+    }
+
+    /**
+     * Function executed before each test
+     */
+    before() {
+    }
+
+    /**
+     * Function executed after each test
+     */
+    after() {
+    }
+
+    /**
+     * Test if universal route returns an error if bad value in module config bootstrap
+     */
+    @test('- check if universal route returns an error if bad value in module config bootstrap')
+    testGetHtmlUniversaleRouteErrorInModuleConfigBootstrap(done) {
+        @HapinessModule({
+            version: '1.0.0',
+            imports: [
+                NgUniversalModule.setConfig({bootstrap: <any> {}})
+            ]
+        })
+        class NUMTest {}
+
+        Hapiness.bootstrap(NUMTest, [
+            HttpServerExt.setConfig({ host: '0.0.0.0', port: 4443 })
+        ]).then(_ => {
+            const server = Hapiness['extensions'].pop().value;
+            server.inject('./test.html', reply => unit.exception(reply.result).when(__ => server.stop().then(___ => done())));
+        });
+    }
+
+    /**
+     * Test if universal route returns an error if no module config
+     */
+    @test('- check if universal route returns an error if no module config')
+    testGetHtmlUniversaleRouteErrorOnModuleBootstrap(done) {
+        @HapinessModule({
+            version: '1.0.0',
+            imports: [
+                NgUniversalModule
+            ]
+        })
+        class NUMTest {}
+
+        Hapiness.bootstrap(NUMTest, [
+            HttpServerExt.setConfig({ host: '0.0.0.0', port: 4443 })
+        ]).then(_ => {
+            const server = Hapiness['extensions'].pop().value;
+            server.inject('./test.html', reply => unit.exception(reply.result).when(__ => server.stop().then(___ => done())));
+        });
+    }
+}
